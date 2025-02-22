a980540e8b97fa93317a9f60005a9fd724c95f3b
Faultz <notmyfaultv5@hotmail.com>
2025-02-22 18:56:32 +0000

    fix video out hook

--- liborbisrender/source/renderer/hooks/sce_video.cpp
+++ liborbisrender/source/renderer/hooks/sce_video.cpp
@@ -7,16 +7,12 @@ int render_context::sceVideoOutSubmitFlip_h(uint32_t videoOutHandle, uint32_t di
 	auto context = get_instance();
 
 	if (context->flags & UnlockFps)
-		sceVideoOutSetFlipRate(context->video_out_handle, 0);
-
-	auto should_render_after_flip = context->flags & RenderAfterFlip;
-	if (!should_render_after_flip && (context->flags & StateDestroying) == 0)
-	{
-		if (context->user_callback)
-			context->user_callback(displayBufferIndex);
-	}
+		sceVideoOutSetFlipRate(videoOutHandle, 0);
 
 	auto res = context->sceVideoOutSubmitFlip_d.invoke<int>(videoOutHandle, displayBufferIndex, flipMode, flipArg);
+
+	if (context->user_callback)
+		context->user_callback((displayBufferIndex + 1) % context->get_target_count());
 
 	while (*context->curr_frame_context->context_label != label_free)
 	{
