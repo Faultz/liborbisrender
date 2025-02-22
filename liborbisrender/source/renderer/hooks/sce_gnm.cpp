462d3abe6288c1a38d3e3beba9504534926bf549
Faultz <notmyfaultv5@hotmail.com>
2025-02-22 18:56:43 +0000

    remove render after flip checks

--- liborbisrender/source/renderer/hooks/sce_gnm.cpp
+++ liborbisrender/source/renderer/hooks/sce_gnm.cpp
@@ -9,12 +9,8 @@ int render_context::sceGnmSubmitAndFlipCommandBuffers_h(uint32_t count, void* dc
 	if (context->flags & UnlockFps)
 		sceVideoOutSetFlipRate(context->video_out_handle, 0);
 
-	auto should_render_after_flip = context->flags & RenderAfterFlip;
-	if (!should_render_after_flip && (context->flags & StateDestroying) == 0)
-	{
-		if (context->user_callback)
-			context->user_callback(displayBufferIndex);
-	}
+	if (context->user_callback)
+		context->user_callback(displayBufferIndex);
 
 	auto res = context->sceGnmSubmitAndFlipCommandBuffer_d.invoke<int>(count, dcbGpuAddrs, dcbSizesInBytes, ccbGpuAddrs, ccbSizesInBytes, videoOutHandle, displayBufferIndex, flipMode, flipArg);
 
@@ -41,12 +37,8 @@ int render_context::sceGnmSubmitAndFlipCommandBuffersForWorkload_h(int workloadI
 	if (context->flags & UnlockFps)
 		sceVideoOutSetFlipRate(context->video_out_handle, 0);
 
-	auto should_render_after_flip = context->flags & RenderAfterFlip;
-	if (!should_render_after_flip && (context->flags & StateDestroying) == 0)
-	{
-		if (context->user_callback)
-			context->user_callback(displayBufferIndex);
-	}
+	if (context->user_callback)
+		context->user_callback(displayBufferIndex);
 
 	auto res = context->sceGnmSubmitAndFlipCommandBufferForWorkload_d.invoke<int>(workloadId, count, dcbGpuAddrs, dcbSizesInBytes, ccbGpuAddrs, ccbSizesInBytes, videoOutHandle, displayBufferIndex, flipMode, flipArg);
 
